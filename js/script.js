d4a9689773a4887819ad2f77c39046cc769de0bb
Ondřej Malík <122880974+AndyMalikk@users.noreply.github.com>
2023-06-06 20:12:10 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-const slider = document.querySelector('#image-slider');
-const arrLeft = document.querySelector('.arrow-left');
-const arrRight = document.querySelector('.arrow-right');
-const header = document.querySelector('.caption h1');
-const description = document.querySelector('.caption p');
+const slider = document.querySelector("#image-slider");
+const arrLeft = document.querySelector(".arrow-left");
+const arrRight = document.querySelector(".arrow-right");
+const header = document.querySelector(".caption h1");
+const description = document.querySelector(".caption p");
 
 const images = [
-  "imgs/OpenGym.jpg", "imgs/StrongmanGym.png", "imgs/Recepce.png"
+  "imgs/OpenGym.jpg",
+  "imgs/StrongmanGym.png",
+  "imgs/Recepce.png",
 ];
-const headings = [
-  'Fitness Gym', 'Open Gym', 'Reception'
-];
+const headings = ["Fitness Gym", "Open Gym", "Reception"];
 const descriptions = [
-  'A gym with lots of free weights and some great machines.', 'A gym for strongmen to lift heavy-ahh weights.', 'Nice and spacious reception.'
+  "A gym with lots of free weights and some great machines.",
+  "A gym for strongmen to lift heavy-ahh weights.",
+  "Nice and spacious reception.",
 ];
 
 let id = 0;
 
 function slide(id) {
   slider.style.backgroundImage = `url(${images[id]})`;
-  slider.classList.add('image-fade');
+  slider.classList.add("image-fade");
   setTimeout(() => {
-    slider.classList.remove('image-fade');
+    slider.classList.remove("image-fade");
   }, 550);
   header.innerText = headings[id];
   description.innerText = descriptions[id];
@@ -29,7 +31,7 @@ function slide(id) {
 // Initial call to slide function with the initial id
 slide(id);
 
-arrLeft.addEventListener('click', () => {
+arrLeft.addEventListener("click", () => {
   id--;
   if (id < 0) {
     id = images.length - 1;
@@ -37,61 +39,54 @@ arrLeft.addEventListener('click', () => {
   slide(id);
 });
 
-arrRight.addEventListener('click', () => {
+arrRight.addEventListener("click", () => {
   id++;
   if (id > images.length - 1) {
     id = 0;
   }
   slide(id);
 });
- 
 
-    /*SCROLL ANIMATION*/
+/*SCROLL ANIMATION*/
 
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "70px",
+  duration: 800,
+  delay: 100,
+});
 
-    
-    const sr = ScrollReveal({
-      origin: 'top',
-      distance: '70px',
-      duration: 800,
-      delay: 100  , 
-    })
+sr.reveal(".intro-name");
+sr.reveal(".intro-hashtag", { delay: 300, origin: "bottom" });
+sr.reveal(".quote, #workouts, #prices", { interval: 100 });
+sr.reveal(".author, #image-slider, .map, .container", {
+  interval: 100,
+  origin: "bottom",
+});
+sr.reveal(".footer-text", { interval: 100, origin: "left" });
 
-  sr.reveal('.intro-name')
-  sr.reveal('.intro-hashtag',{delay:300, origin:'bottom'})
-  sr.reveal('.quote, #workouts, #prices',{interval: 100})
-  sr.reveal('.author, #image-slider, .map, .container',{interval: 100, origin: 'bottom'})
-  sr.reveal('.footer-text',{interval: 100, origin: 'left'})
+function calculateBmi() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
 
-  function calculateBmi() {
-    let weight = document.getElementById('weight').value
-    let height = document.getElementById('height').value
-    
-    let bmi = weight / (height / 100) ** 2
-    
-    document.getElementById('heading').innerHTML = 'Your BMI is:'
-    document.getElementById('bmi-output').innerHTML = bmi.toFixed(2)
-    
-    if (bmi <= 24.9) {
-      document.getElementById('message').innerHTML = 'You are underweight'
-    } else if (bmi >= 25 && bmi <= 29.9) {
-      document.getElementById('message').innerHTML = 'Your weight is normal'
-    } else {
-      document.getElementById('message').innerHTML = 'You are overweight'
-    }
+  let bmi = weight / (height / 100) ** 2;
+
+  document.getElementById("heading").innerHTML = "Your BMI is:";
+  document.getElementById("bmi-output").innerHTML = bmi.toFixed(2);
+
+  if (bmi <= 24.9) {
+    document.getElementById("message").innerHTML = "You are underweight";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    document.getElementById("message").innerHTML = "Your weight is normal";
+  } else {
+    document.getElementById("message").innerHTML = "You are overweight";
   }
-
-  function reload() {
-    document.getElementById('weight').value = ''
-  document.getElementById('height').value = ''
-  document.getElementById('heading').innerHTML = ''
-  document.getElementById('bmi-output').innerHTML = ''
-  document.getElementById('message').innerHTML = ''
 }
 
-
-
-
-
-
-
+function reload() {
+  document.getElementById("weight").value = "";
+  document.getElementById("height").value = "";
+  document.getElementById("heading").innerHTML = "";
+  document.getElementById("bmi-output").innerHTML = "";
+  document.getElementById("message").innerHTML = "";
+}
