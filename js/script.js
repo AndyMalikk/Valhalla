d85900be4bb8499e83506ed403c79647ef2b5314
Ondřej Malík <122880974+AndyMalikk@users.noreply.github.com>
2024-04-16 19:02:07 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -64,29 +64,3 @@ sr.reveal(".author, #image-slider, .map, .container", {
   origin: "bottom",
 });
 sr.reveal(".footer-text", { interval: 100, origin: "left" });
-
-function calculateBmi() {
-  let weight = document.getElementById("weight").value;
-  let height = document.getElementById("height").value;
-
-  let bmi = weight / (height / 100) ** 2;
-
-  document.getElementById("heading").innerHTML = "Your BMI is:";
-  document.getElementById("bmi-output").innerHTML = bmi.toFixed(2);
-
-  if (bmi <= 24.9) {
-    document.getElementById("message").innerHTML = "You are underweight";
-  } else if (bmi >= 25 && bmi <= 29.9) {
-    document.getElementById("message").innerHTML = "Your weight is normal";
-  } else {
-    document.getElementById("message").innerHTML = "You are overweight";
-  }
-}
-
-function reload() {
-  document.getElementById("weight").value = "";
-  document.getElementById("height").value = "";
-  document.getElementById("heading").innerHTML = "";
-  document.getElementById("bmi-output").innerHTML = "";
-  document.getElementById("message").innerHTML = "";
-}
